4d11cb86fffa9e06998c22f3e47e1a217df54a0a
Leewonno <dldnjssh123@naver.com>
2025-08-20 06:18:04 +0900

    [Feat] index

--- kpop/settings.py
+++ kpop/settings.py
@@ -79,8 +79,7 @@ ROOT_URLCONF = 'kpop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
